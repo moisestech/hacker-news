96457d0842ba84942fc936c0a260521ee30d08c4
moisestech <m@moises.tech>
2020-09-05 09:37:49 -0400

    entry, output, path, filename, publicPath, module, mode, plugins, devServer

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "_redirects" }],
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
