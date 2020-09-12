adf674f64bdf51d2786b2292235a298e6a7dd4a5
Moises Sanabria <m@moises.tech>
2020-09-12 14:40:12 -0500

    content setContent useState, useEffect setInt content

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
-export default function Loading(props) {
-  return <div className="loading">Loading</div>;
+export default function Loading({ text = "Loading" }) {
+  const [content, setContent] = useState(text);
+
+  useEffect(() => {
+    const id = window.setInterval(() => {
+      setContent((content) => {
+        return content === `${text}...` ? text : `${content}.`;
+      });
+    }, 500);
+
+    return () => window.clearInterval(id);
+  }, [text]);
+
+  return (
+    <div className="container loading">
+      <p className="text-center">{content}</p>
+    </div>
+  );
 }
 
-Loading.propTypes = {};
+Loading.propTypes = {
+  text: PropTypes.string,
+};
