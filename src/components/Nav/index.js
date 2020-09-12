938eb2d19a3a785b9ddd5083c2b8f06e673cb71f
Moises Sanabria <m@moises.tech>
2020-09-12 14:42:11 -0500

    NavHeader NavButtons ThemeButton useContext(ThemeContext)

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,36 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useContext } from "react";
+import ThemeContext from "../../contexts/theme";
+import { NavLink } from "react-router-dom";
 
-export default function Nav(props) {
-  return <div className="nav">Navigation Bar</div>;
+import ThemeButton from "./components/ThemeButton";
+import NavHeader from "./components/NavHeader";
+
+const activeStyle = { color: "white" };
+
+function NavButtons() {
+  return (
+    <ul className="row nav">
+      <li>
+        <NavLink exact to="/" activeStyle={activeStyle} className="nav-link">
+          Top
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/new" activeStyle={activeStyle} className="nav-link">
+          New
+        </NavLink>
+      </li>
+    </ul>
+  );
 }
 
-Nav.propTypes = {};
+export default function Nav({ toggleTheme }) {
+  const theme = useContext(ThemeContext);
+  return (
+    <nav className="row space-between">
+      <NavHeader />
+      <NavButtons />
+      <ThemeButton theme={theme} toggleTheme={toggleTheme} />
+    </nav>
+  );
+}
