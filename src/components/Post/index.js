24ce8ce2ee945d7556b36a293beb05417419ae7f
Moises Sanabria <m@moises.tech>
2020-09-12 14:47:32 -0500

    init func props template

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,8 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
+import * as React from "react";
 
-export default function Post(props) {
-  return <div className="Post">Post</div>;
+export default function Post() {
+  return (
+    <div className="post-page">
+      <h1>Post</h1>
+    </div>
+  );
 }
-
-Post.propTypes = {};
