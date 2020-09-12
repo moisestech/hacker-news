36f95c8706388c32dcbb91d23211bb7f63d848ed
Moises Sanabria <m@moises.tech>
2020-09-12 14:48:28 -0500

    by, score, descendants, time, id {Link}, formatDate

--- src/components/PostMetaInfo/index.js
+++ src/components/PostMetaInfo/index.js
@@ -1,8 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { formatDate } from "../../utils/helpers";
 
-export default function PostMetaInfo(props) {
-  return <div className="post-meta-info">Post Meta Info</div>;
+export default function PostMetaInfo({ by, score, descendants, time, id }) {
+  return (
+    <>
+      <div className="meta-info-light">
+        <span class="subtext">
+          {score} points by{" "}
+          <Link
+            to={{ pathname: "/user", search: `?userId=${by}` }}
+            className="user-link"
+          >
+            {by}
+          </Link>{" "}
+          on {formatDate(time)}{" "}
+          <Link
+            to={{ path: "/post", seearch: `?id=${id}` }}
+            className="post-link"
+          >
+            {descendants} comments
+          </Link>
+        </span>
+      </div>
+    </>
+  );
 }
 
-PostMetaInfo = {};
+PostMetaInfo.propTypes = {
+  by: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  descendants: PropTypes.number.isRequired,
+  time: PropTypes.number.isRequired,
+  url: PropTypes.string,
+};
