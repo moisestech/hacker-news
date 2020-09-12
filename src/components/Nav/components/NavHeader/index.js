a81eee52b46e43ccfd66a4aed00e207a80698247
Moises Sanabria <m@moises.tech>
2020-09-12 14:27:32 -0500

    main header, logotype, and icon

---
+++ src/components/Nav/components/NavHeader/index.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export default function NavHeader() {
+  return (
+    <>
+      <img
+        src="https://news.ycombinator.com/y18.gif"
+        alt="logo for hackernews"
+        className="avatar"
+      />
+      <p className="logo">Hacker News</p>
+    </>
+  );
+}
