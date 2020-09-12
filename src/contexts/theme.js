ba93d70563f039731606991554540b31c024b58d
Moises Sanabria <m@moises.tech>
2020-09-12 14:53:36 -0500

    ThemeContext, ThemeConsumer, ThemeProvider

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -2,5 +2,6 @@ import React from "react";
 
 const ThemeContext = React.createContext();
 
+export default ThemeContext;
 export const ThemeConsumer = ThemeContext.Consumer;
 export const ThemeProvider = ThemeContext.Provider;
