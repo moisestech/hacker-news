35916dfc7d3e6c0a0ea9eb1dcd08570d9181f7f2
Moises Sanabria <m@moises.tech>
2020-09-12 14:35:25 -0500

    individual comment w/ dangerouslySetInnerHTML

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import PostMetaInfo from "../PostMetaInfo";
 
-export default function Comment(props) {
-  return <div className="comment">Comment</div>;
+export default function Comment({ comment }) {
+  return (
+    <div className="comment">
+      <PostMetaInfo
+        comment={true}
+        by={comment.by}
+        time={comment.time}
+        id={comment.id}
+      />
+      <p dangerouslySetInnerHTML={{ __html: comment.text }} />
+    </div>
+  );
 }
 
-Comment.propTypes = {};
+Comment.propTypes = {
+  comment: PropTypes.object.isRequired,
+};
