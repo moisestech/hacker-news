a75599321c908f9829d314af6be66be4f666b6b9
Moises Sanabria <m@moises.tech>
2020-09-12 14:50:48 -0500

    truncate link, title str, link str

--- src/components/PostTitle/index.js
+++ src/components/PostTitle/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function PostTitle(props) {
-  return <div className="post-title">Post Title</div>;
+export default function PostTitle({ title, link }) {
+  function truncate(str, n) {
+    return str.length > n ? str.substr(0, n - 1) + " 🔗" : str;
+  }
+
+  return (
+    <div className="post-title">
+      <a className="link" href={link}>
+        {title}
+      </a>
+      <a className="small-link" href={link}>
+        {link ? `(${truncate(link, 20)})` : null}
+      </a>
+    </div>
+  );
 }
 
-PostTitle.propTypes = {};
+PostTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+  link: PropTypes.string,
+};
